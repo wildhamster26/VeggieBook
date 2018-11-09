6986d70f9e38a9aa922b49d22b35c11e0be65743
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-11-09 17:42:10 +0100

    Added user views

--- VeggieBook/models/User.js
+++ VeggieBook/models/User.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  username: {type: String, unique: true, required: true},
+  password: {type: String, required: true, min: 3},
+  email: {type: String, unique: true, required: true, match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/},
+  //Kind of vegetable
+  kind: String,
+  age: Number,
+  phoneNumber: String,
+  hobbies: String,
+  fears: String,
+  //favorite foods to be in
+  favFoods: String,
+  darkSecret: String
 }, {
   timestamps: {
     createdAt: 'created_at',
--- VeggieBook/routes/index.js
+++ VeggieBook/routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const User = require("../models/User")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/users', (req, res, next) => {			
+	// Get all the users from the db		
+	User.find()		
+	.then(users => {		
+		res.render("users", {	
+			users: users
+		})	
+	})		
+});				
 
 module.exports = router;
