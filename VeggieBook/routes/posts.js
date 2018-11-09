60cac6afed55bab86e90411ffa19b2d7377a8caa
carloscadel <c.cazallas.delfa@gmail.com>
2018-11-09 17:45:26 +0100

    Add post.js and Commnt Model

---
+++ VeggieBook/routes/posts.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+/* Will include routes to posts and comments */
+
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+
+module.exports = router;
