7683669604281e5b0f0f4320f08f02d1a3d507b3
carloscadel <c.cazallas.delfa@gmail.com>
2018-11-13 16:14:39 +0100

    friend request email is sent

--- app.js
+++ app.js
@@ -127,8 +127,7 @@ app.use((req,res, next) => {
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
 app.use('/posts', require('./routes/posts'));
-app.use('/find-friends', require('./routes/find-friends'));
-app.use('/my-friends', require('./routes/my-friends'));
+app.use('/friends', require('./routes/friends'));
 app.use('/events', ensureLoggedIn() , require('./routes/events'));
 
       
